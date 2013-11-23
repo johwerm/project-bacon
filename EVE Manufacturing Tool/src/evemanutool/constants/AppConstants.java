4bcc0b6be5ec80716f5c3ed4e9e4e94f3ff8e716
Johan Wermensjö <johanwermensjoe@gmail.com>
2013-11-23 19:11:09 +0100

    Set the current version to be 1.04 Alpha.

--- EVE Manufacturing Tool/src/evemanutool/constants/AppConstants.java
+++ EVE Manufacturing Tool/src/evemanutool/constants/AppConstants.java
@@ -3,7 +3,7 @@ package evemanutool.constants;
 public interface AppConstants {
 
 	//Version.
-	public static final String APP_VERSION = "1.04";
+	public static final String APP_VERSION = "1.04 Alpha";
 
 	//Credits text.
 	public static final String CREDITS_TEXT = 	"Icons - Visual Pharm\n" +
