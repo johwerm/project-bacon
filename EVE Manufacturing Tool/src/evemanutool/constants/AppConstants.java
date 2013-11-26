0a8b6f687f8869e86cdadf4df0dc252fca21de1d
Johan Wermensjö <johanwermensjoe@gmail.com>
2013-11-26 22:14:29 +0100

    Stable v 1.05 Alpha Finished.

--- EVE Manufacturing Tool/src/evemanutool/constants/AppConstants.java
+++ EVE Manufacturing Tool/src/evemanutool/constants/AppConstants.java
@@ -3,7 +3,7 @@ package evemanutool.constants;
 public interface AppConstants {
 
 	//Version.
-	public static final String APP_VERSION = "1.04 Alpha";
+	public static final String APP_VERSION = "1.05 Alpha";
 	
 	//Window icon.
 	public static final String WINDOW_ICON_PATH = "img/emt_icon.png";
